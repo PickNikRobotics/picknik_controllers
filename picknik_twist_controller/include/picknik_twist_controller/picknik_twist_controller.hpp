8d25fbb53863b1ecf82f50b91801b2b875424d67
Sebastian Castro <4603398+sea-bass@users.noreply.github.com>
2025-02-05 23:52:00 -0500

    Update picknik_twist_controller.hpp

--- picknik_twist_controller/include/picknik_twist_controller/picknik_twist_controller.hpp
+++ picknik_twist_controller/include/picknik_twist_controller/picknik_twist_controller.hpp
@@ -31,7 +31,6 @@
 #include "controller_interface/controller_interface.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "picknik_twist_controller/visibility_control.h"
-#include <rclcpp/version.h>
 #include "realtime_tools/realtime_buffer.hpp"
 
 namespace picknik_twist_controller
